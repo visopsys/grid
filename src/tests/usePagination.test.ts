73b6777e7298e3d6fc032adf17f46a23a9250531
Vinay <rmdort@gmail.com>
2020-06-06 09:04:56 +0800

    Tests refactor

--- src/tests/usePagination.test.ts
+++ src/tests/usePagination.test.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { renderHook, act } from "@testing-library/react-hooks";
 import usePagination from "../hooks/usePagination";
 
--- src/tests/useSelection.test.ts
+++ src/tests/useSelection.test.ts
@@ -12,7 +12,9 @@ describe("useSelection", () => {
   });
 
   it("picks up initialselection", () => {
-    const initialSelections = [{ top: 0, left: 0, right: 0, bottom: 0 }];
+    const initialSelections = [
+      { bounds: { top: 0, left: 0, right: 0, bottom: 0 } },
+    ];
     const { result } = renderHook(() =>
       useSelection({
         initialSelections,
