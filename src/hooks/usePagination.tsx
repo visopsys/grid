e2b8fbd74676b5c26e1bc307eaa263aede8790d3
Vinay <rmdort@gmail.com>
2020-06-09 22:53:03 +0800

    Add code comments

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -1,26 +1,71 @@
 import React, { useState, useEffect, isValidElement } from "react";
 
 export interface PaginationProps {
+  /**
+   * No of items in each page
+   */
   pageSize: number;
+  /**
+   * Initial current page index. Start from 1
+   */
   initialCurrentPage?: number;
+  /**
+   * Total number of rows
+   */
   total: number;
+  /**
+   * Callback when page is changed
+   */
   onChange?: (page: number) => void;
+  /**
+   * Pagination Component
+   */
   component?:
     | React.FC<PaginationComponentProps>
     | React.ComponentClass<PaginationComponentProps>;
 }
 
-type PaginationComponentProps = Omit<PaginationResults, "paginationComponent">;
+export type PaginationComponentProps = Omit<
+  PaginationResults,
+  "paginationComponent"
+>;
 
 export interface PaginationResults {
+  /**
+   * Current page number
+   */
   currentPage: number;
+  /**
+   * Total pages
+   */
   totalPages: number;
+  /**
+   * No of items in each page
+   */
   pageSize: number;
+  /**
+   * Pagination component
+   */
   paginationComponent: React.ReactNode;
+  /**
+   * Navigate to next page
+   */
   nextPage: () => void;
+  /**
+   * Navigate to prev page
+   */
   prevPage: () => void;
+  /**
+   * Navigate to first page
+   */
   goToFirst: () => void;
+  /**
+   * Navigate to last page
+   */
   goToLast: () => void;
+  /**
+   * Navigate to specific page
+   */
   goToPage: (page: number) => void;
 }
 
