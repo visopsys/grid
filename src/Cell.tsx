a4018e07a821614d51998baa5c59ba3e4b762ef2
Vinay <rmdort@gmail.com>
2020-06-12 16:15:57 +0800

    Modify cell

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -60,7 +60,11 @@ const Cell: React.FC<CellProps> = memo((props) => {
             context.font = `${fontSize}px ${fontFamily}`;
             // @ts-ignore
             context.fillStyle = textColor;
-            context.fillText(value, padding, (height || 0) - padding);
+            const lineTranslateX = align === "center" ? (width || 0) / 2 : x;
+            // @ts-ignore
+            context.textAlign = align;
+            context.fillText(value, lineTranslateX, (height || 0) - padding);
+            // context.fillText(value, padding, (height || 0) - padding);
           }}
         />
       ) : null}
