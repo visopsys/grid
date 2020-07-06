e82e6dc8a508e62d63d26ab55ae55085eb43e562
Vinay <rmdort@gmail.com>
2020-07-07 01:07:01 +0800

    Fix exports

--- packages/spreadsheet/src/index.tsx
+++ packages/spreadsheet/src/index.tsx
@@ -2,7 +2,6 @@ import Spreadsheet, { defaultSheets } from "./Spreadsheet";
 import Editor from "./Editor";
 import Cell from "./Cell";
 export * from "./Spreadsheet";
-export { RendererProps } from "@rowsncolumns/grid";
 export default Spreadsheet;
 export { Editor as DefaultEditor, Cell as DefaultCell };
 export { defaultSheets }
