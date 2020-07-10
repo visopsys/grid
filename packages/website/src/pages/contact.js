9f693e5ee42fc3e44e30084c0a6f287808cc9d2d
Vinay <rmdort@gmail.com>
2020-07-10 16:23:59 +0800

    Added atlassian

--- packages/website/src/pages/contact.js
+++ packages/website/src/pages/contact.js
@@ -21,13 +21,36 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout title="Contact" description="Contact rowsncolumns">
-      <Box className="container" pt={10} display='block' pb={10}>
-        <Text fontSize={30} fontWeight='bold' mb={5} as='h1'>Contact us</Text>
-        <Text fontSize={20} fontWeight='bold' as='h2' mb={1}>Sales enquiries</Text>
-        <Text>For sales enquiries, email us at <a href='mailto:sales@rowsncolumns.app'>sales@rowsncolumns.app</a></Text>
+      <Box className="container" pt={10} display="block" pb={10}>
+        <Text fontSize={30} fontWeight="bold" mb={5} as="h1">
+          Contact us
+        </Text>
+        <Text fontSize={20} fontWeight="bold" as="h2" mb={1}>
+          Sales enquiries
+        </Text>
+        <Text>
+          For sales enquiries, email us at{" "}
+          <a href="mailto:sales@rowsncolumns.app">sales@rowsncolumns.app</a>
+        </Text>
 
-        <Text mt={5} fontSize={20} fontWeight='bold' as='h2' mb={1}>Support</Text>
-        <Text>For support, email us at <a href='mailto:support@rowsncolumns.app'>support@rowsncolumns.app</a></Text>
+        <Text mt={5} fontSize={20} fontWeight="bold" as="h2" mb={2}>
+          Support
+        </Text>
+
+        <Button
+          target="_blank"
+          variantColor="green"
+          _hover={{ color: "white" }}
+          as="a"
+          href="https://rowsncolumns.atlassian.net/servicedesk/customer/portal/3"
+        >
+          Submit a support ticket
+        </Button>
+
+        <Text mt={2} mb={2}>
+          Or, email us at{" "}
+          <a href="mailto:support@rowsncolumns.app">support@rowsncolumns.app</a>
+        </Text>
       </Box>
     </Layout>
   );
